3ce7cc93917025b3af436d8a2018b62e12666d89
A491382 <371937605@qq.com>
2017-04-20 11:44:50 +0800

    fix progress bar issue

--- static/js/lib/index.js
+++ static/js/lib/index.js
@@ -44,46 +44,18 @@ $('.point').on('click', function(e) {
   var getTotalPoints = $('.point').length,
     getIndex = $(this).index(),
     getCompleteIndex = $('.point--active').index();
-
-  TweenMax.to($('.bar__fill'), 0.6, {
-    width: (getIndex - 1) / (getTotalPoints - 1) * 100 + '%'
-  });
-
-  if (getIndex => getCompleteIndex) {
     $('.point--active').addClass('point--complete').removeClass('point--active');
-
     $(this).addClass('point--active');
-    $(this).prevAll().addClass('point--complete');
-    $(this).nextAll().removeClass('point--complete');
-  }
+    $(this).prevAll().removeClass('point--complete').removeClass('point--active');
+    $(this).nextAll().removeClass('point--complete').removeClass('point--active');
+	
+	//change progress bar label
+	$('.progress-note-label:nth-child('+(getIndex+1)+')').removeClass('hide');
+	$('.progress-note-label:nth-child('+(getIndex+1)+')').prevAll().addClass('hide');
+	$('.progress-note-label:nth-child('+(getIndex+1)+')').nextAll().addClass('hide');
 });
 
-/*
-  Demo Purposes
-*/
-var progressAnimation = function() {
-  var getTotalPoints = $('.point').length,
-    getIndex = Math.floor(Math.random() * 4) + 1,
-    getCompleteIndex = $('.point--active').index();
 
-  TweenMax.to($('.bar__fill'), 0.6, {
-    width: (getIndex - 1) / (getTotalPoints - 1) * 100 + '%'
-  });
-
-  if (getIndex => getCompleteIndex) {
-    $('.point--active').addClass('point--complete').removeClass('point--active');
-
-    $('.point:nth-child(' + (getIndex + 1) + ')').addClass('point--active');
-    $('.point:nth-child(' + (getIndex + 1) + ')').prevAll().addClass('point--complete');
-    $('.point:nth-child(' + (getIndex + 1) + ')').nextAll().removeClass('point--complete');
-  }
-};
-
-var animateProgress = setInterval(progressAnimation, 1200);
-
-$(document).hover(function() {
-  clearInterval(animateProgress)
-});
 
 $('.radius-toggle').on('click', function() {
   $('body').toggleClass('show-radius')
